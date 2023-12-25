edb8d94f045038abecd673e72b0ae6524082a90a
Nikunj Beladiya <ndbeladiya720@gmail.com>
2023-12-25 13:06:59 +0530

    setup middleware, health route and 404 route

--- server.ts
+++ server.ts
@@ -1,10 +1,32 @@
-import express from "express";
+import cors from "cors";
+import express, { Request, Response } from "express";
+import morgan from "morgan";
 
 const app = express();
 
-app.get('/', (req, res) => {
-	res.json({status: 200, message: "App is running "})
-})
+// Middleware 
+app.use(morgan("dev"))
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    credentials: true
+}));
+app.use(express.json())
+
+// Health route
+app.get("/api/health", (req: Request, res: Response) => {
+    res.status(200).json({
+        status: "success",
+        message: "2FA app is running, Health status is OK"
+    })
+});
+
+
+app.all("*", (req: Request, res: Response) => {
+    res.status(404).json({
+        status: "fail",
+        message: `Route ${req.originalUrl} is not found`
+    })
+});
 
 app.listen(3000, () => {
     console.log("Server is Successfully Running, and App is listening on port "+ 3000)
