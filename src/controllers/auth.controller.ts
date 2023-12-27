d12dc734545e76dd207b5d6e0768cf618b3ef5f0
Nikunj Beladiya <ndbeladiya720@gmail.com>
2023-12-27 12:30:40 +0530

    create user implementation

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,11 +1,28 @@
 import { Request, Response } from "express";
+import User, { IUser } from "../models/user";
+
 
 const RegisterUser = async (req: Request, res: Response) => {
-    console.log(req.body)
-    res.status(201).json({
-        status: "success", 
-        message: "User created successfully"
-    })
+   
+    try {
+        const reqData: IUser = req.body;
+        if(await User.isEmailUsed(req.body.email) ) {
+            return res.status(404).json({
+                status: "fail", 
+                message: `User already exist with ${reqData.email}`
+            })
+        }
+        await User.create(reqData)
+        res.status(201).json({
+            status: "success", 
+            message: "User created successfully"
+        });
+    } catch (error: any) {
+        res.status(404).json({
+            status: "fail", 
+            message: error.message
+        })
+    }
 }
 
 
